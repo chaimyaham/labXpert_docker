540f27864b5c1de3aa33259f36d52f58067f6d63
Chaimaa mahy <chaim.yaham17@gmail.com>
2024-02-06 21:49:27 +0100

    adding jwtfilter

--- src/main/java/com/aitnacer/LabXpert/entity/Utilisateur.java
+++ src/main/java/com/aitnacer/LabXpert/entity/Utilisateur.java
@@ -1,10 +1,15 @@
 package com.aitnacer.LabXpert.entity;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
 
@@ -14,7 +19,7 @@ import java.util.List;
 @Entity
 @Table(name = "Utilisateur")
 @NoArgsConstructor
-public class Utilisateur extends UtilisateurInfo {
+public class Utilisateur extends UtilisateurInfo implements UserDetails {
     @NotNull(message = "The username should not be null!")
     private String userName;
     @NotNull(message = "The password should not be null!")
@@ -26,5 +31,35 @@ public class Utilisateur extends UtilisateurInfo {
     @Builder
     public Utilisateur(Long id, String nom, String prenom, String Adresse, String telephone, EnumSexe sexe, boolean deleted, String userName, String password, UserRole role) {
 
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
